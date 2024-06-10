768c3bb3f0dbb7faefc0ee5a61cadd17e851e95a
agufedee <agufedee@gmail.com>
2024-06-10 18:22:09 -0300

    propiedades en constructor

--- Ej_1.php
+++ Ej_1.php
@@ -3,23 +3,9 @@ include 'tablas.php';
 class Elementos
 {
     public $atributos = ['Titulo', 'Autor', 'Pais de Origen', 'Tamaño en Mb', 'Año de Publicacion', 'Idioma', 'Formato'];
-    public $titulo;
-    public $autor;
-    public $pais_origen;
-    public $tamaño_mb;
-    public $año_publicacion;
-    public $idioma;
-    public $formato;
-
-    public function __construct(string $titulo = '', string $autor = '', int $año_publicacion, string $pais_origen = '', string $idioma = '', int $tamaño_mb, string $formato = '')
+    
+    public function __construct(public string $titulo = '', public string $autor = '', public int $año_publicacion, public string $pais_origen = '', public string $idioma = '', public int $tamaño_mb, public string $formato = '')
     {
-        $this->titulo = $titulo;
-        $this->autor = $autor;
-        $this->año_publicacion = $año_publicacion;
-        $this->pais_origen = $pais_origen;
-        $this->idioma = $idioma;
-        $this->tamaño_mb = $tamaño_mb;
-        $this->formato = $formato;
     }
     public function obtener_valores()
     {
@@ -38,17 +24,9 @@ class Elementos
 class Canciones extends Elementos
 {
     public $atributos_propios = ['Género', 'Reproducciones', 'Album'];
-    public $genero;
-    public $reproducciones;
-    public $album;
 
-
-    public function __construct(string $titulo = '', string $autor = '', int $año_publicacion = 0, string $pais_origen = '', string $idioma = '', int $tamaño_mb = 0, string $formato = '', string $genero = '', int $reproducciones = 0, string $album = '')
+    public function __construct(public string $titulo = '', public string $autor = '', public int $año_publicacion = 0, public string $pais_origen = '', public string $idioma = '', public int $tamaño_mb = 0, public string $formato = '', public string $genero = '', public int $reproducciones = 0, public string $album = '')
     {
-        parent::__construct($titulo, $autor, $año_publicacion, $pais_origen, $idioma, $tamaño_mb, $formato);
-        $this->genero = $genero;
-        $this->reproducciones = $reproducciones;
-        $this->album = $album;
         $this->atributos = array_merge($this->atributos, $this->atributos_propios);
     }
     public function obtener_valores()
@@ -63,15 +41,9 @@ class Canciones extends Elementos
 class Libros extends Elementos
 {
     public $atributos_propios = ['Editorial', 'Genero', 'Precio'];
-    public $editorial;
-    public $genero;
-    public $precio;
-    public function __construct(string $titulo, string $autor, int $año_publicacion, string $pais_origen, string $idioma, int $tamaño_mb, string $formato = '', string $editorial, string $genero, int $precio)
+
+    public function __construct(public string $titulo, public string $autor, public int $año_publicacion, public string $pais_origen, public string $idioma, public int $tamaño_mb, public string $formato = '', public string $editorial, public string $genero, public int $precio)
     {
-        parent::__construct($titulo, $autor, $año_publicacion, $pais_origen, $idioma, $tamaño_mb, $formato);
-        $this->editorial = $editorial;
-        $this->genero = $genero;
-        $this->precio = $precio;
         $this->atributos = array_merge($this->atributos, $this->atributos_propios);
     }
     public function obtener_valores()
@@ -86,16 +58,10 @@ class Libros extends Elementos
 class Fotos extends Elementos
 {
     public $atributos_propios = ['Dispositivo', 'Color', 'Resolucion'];
-    public $dispositivo;
-    public $color;
-    public $resolucion;
 
-    public function __construct(string $titulo, string $autor, int $año_publicacion, string $pais_origen, string $idioma, int $tamaño_mb, string $formato = '', string $dispositivo, bool $color, string $resolucion)
+
+    public function __construct(public string $titulo, public string $autor, public int $año_publicacion, public string $pais_origen, public string $idioma, public int $tamaño_mb, public string $formato = '', public string $dispositivo, public bool $color, public string $resolucion)
     {
-        parent::__construct($titulo, $autor, $año_publicacion, $pais_origen, $idioma, $tamaño_mb, $formato);
-        $this->dispositivo = $dispositivo;
-        $this->color = $color;
-        $this->resolucion = $resolucion;
         $this->atributos = array_merge($this->atributos, $this->atributos_propios);
     }
     public function obtener_valores()
@@ -111,16 +77,8 @@ class Videos extends Elementos
 {
     public $atributos_propios = ['Duración', 'Reproducciones', 'Relacion-Aspecto'];
 
-    public $duracion;
-    public $reproducciones;
-    public $relacion_aspecto;
-
-    public function __construct(string $titulo, string $autor, int $año_publicacion, string $pais_origen, string $idioma, int $tamaño_mb, string $formato = '', string $duracion, int $reproducciones, string $relacion_aspecto)
+    public function __construct(public string $titulo, public  string $autor, public int $año_publicacion, public string $pais_origen, public string $idioma, public int $tamaño_mb, public string $formato = '', public string $duracion, public int $reproducciones, public string $relacion_aspecto)
     {
-        parent::__construct($titulo, $autor, $año_publicacion, $pais_origen, $idioma, $tamaño_mb, $formato);
-        $this->duracion = $duracion;
-        $this->reproducciones = $reproducciones;
-        $this->relacion_aspecto = $relacion_aspecto;
         $this->atributos = array_merge($this->atributos, $this->atributos_propios);
     }
     public function obtener_valores()
@@ -136,17 +94,8 @@ class Juegos extends Elementos
 {
     public $atributos_propios = ['Plataforma', 'Modo-Juego', 'Requerimiento-RAM'];
 
-    public $plataforma;
-    public $modo_juego;
-
-    public $requerimento_ram_mb;
-
-    public function __construct(string $titulo, string $autor, int $año_publicacion, string $pais_origen, string $idioma, int $tamaño_mb, string $formato = '', string $plataforma, string $modo_juego, int $requerimento_ram_mb)
+    public function __construct(public string $titulo, public string $autor, public int $año_publicacion, public string $pais_origen, public string $idioma, public int $tamaño_mb, public string $formato = '', public string $plataforma, public string $modo_juego, public int $requerimento_ram_mb)
     {
-        parent::__construct($titulo, $autor, $año_publicacion, $pais_origen, $idioma, $tamaño_mb, $formato);
-        $this->plataforma = $plataforma;
-        $this->modo_juego = $modo_juego;
-        $this->requerimento_ram_mb = $requerimento_ram_mb;
         $this->atributos = array_merge($this->atributos, $this->atributos_propios);
     }
     public function obtener_valores()
@@ -159,17 +108,16 @@ class Juegos extends Elementos
     }
 }
 
-
 //?Instancias de clases
 
 $tabla = new Tabla();
 
-//?Canciones
+//?-------------CANCIONES-------------------
 $cancion1 = new Canciones('chau', 'NTVG', 2011, 'Uruguay', 'español', 8.2, 'digital', 'rock', 5000000, 'Por lo Menos hoy');
 $cancion2 = new Canciones('mariposa tecknicolor', 'Fito Páez', 2011, 'Argentina', 'español', 8.2, 'digital', 'pop/rock', 5000000, 'Circo Beat');
 $cancion3 = new Canciones('Mejor que Ayer', 'Diego Torres', 2011, 'Argentina', 'español', 8.2, 'digital', 'pop', 5000000, 'Mejor que Ayer');
-//Fila de 
-$tipo_contenido = new Fila(['','','','','Canciones','','','','']);
+
+$tipo_contenido = new Fila(['', '', '', '', 'Canciones', '', '', '', '']);
 $tipo_contenido->cambiar_a_titulo(true);
 $tabla->agregar_fila($tipo_contenido);
 
@@ -181,13 +129,12 @@ $tabla->agregar_fila(new Fila($cancion1->obtener_valores()));
 $tabla->agregar_fila(new Fila($cancion2->obtener_valores()));
 $tabla->agregar_fila(new Fila($cancion3->obtener_valores()));
 
-
-//?LIBROS
+//?-----------LIBROS--------------
 $libro1 = new Libros('Biblia', 'Dios', 1960, '', 'español', 0, 'fisico/papel', 'Reina Valera', 'espiritual', 10000);
 $libro2 = new Libros('Harry Potter 1', 'J.K. Rowling', 1997, 'Inglaterra', 'ingles', 0, 'fisico/papel', 'Salamandra', 'fantasia', 14799);
 $libro3 = new Libros('divergente', 'Veronica Roth', 2011, 'EE.UU', 'ingles', 0, 'fisico/papel', 'pepe', 'fantasia', 15000);
 
-$tipo_contenido = new Fila(['','','','','Libros','','','','']);
+$tipo_contenido = new Fila(['', '', '', '', 'Libros', '', '', '', '']);
 $tipo_contenido->cambiar_a_titulo(true);
 $tabla->agregar_fila($tipo_contenido);
 
@@ -204,7 +151,7 @@ $foto1 = new Fotos('arcoiris', 'NTVG', 2011, 'Uruguay', 'español', 3, 'digital'
 $foto2 = new Fotos('cielo azul', 'fedee', 2014, 'españa', 'español', 4.5, 'digital', 'xiaomi M9t', true, '600x600');
 $foto3 = new Fotos('distraccion', 'pedro', 2000, 'peru', 'español', 8, 'impresa', 'sony', false, '2440x720');
 
-$tipo_contenido = new Fila(['','','','','Fotos','','','','']);
+$tipo_contenido = new Fila(['', '', '', '', 'Fotos', '', '', '', '']);
 $tipo_contenido->cambiar_a_titulo(true);
 $tabla->agregar_fila($tipo_contenido);
 
@@ -221,7 +168,7 @@ $video1 = new Videos('pepe', 'NTVG', 2011, 'Uruguay', 'español', 8.2, 'digital'
 $video2 = new Videos('pedro', 'NTVG', 2011, 'Uruguay', 'español', 7.2, 'digital', '3min', 500, '16:9');
 $video3 = new Videos('chau', 'NTVG', 2011, 'Uruguay', 'español', 8.2, 'digital', '3min', 1000, '4:3');
 
-$tipo_contenido = new Fila(['','','','','VIdeos','','','','']);
+$tipo_contenido = new Fila(['', '', '', '', 'VIdeos', '', '', '', '']);
 $tipo_contenido->cambiar_a_titulo(true);
 $tabla->agregar_fila($tipo_contenido);
 
@@ -233,13 +180,12 @@ $tabla->agregar_fila(new Fila($video1->obtener_valores()));
 $tabla->agregar_fila(new Fila($video2->obtener_valores()));
 $tabla->agregar_fila(new Fila($video3->obtener_valores()));
 
-
 //?JUEGOS
 $juego1 = new Juegos('Fifa', 'EA', 1993, 'Japón', 'japones', 35, 'cartucho', 'Multiplataforma', 'historia/multijugador', 32);
 $juego2 = new Juegos('Contra', 'Konami', 1987, 'Japón', 'japones', 10, 'cartucho', 'Arcade', 'historia', 16);
 $juego3 = new Juegos('Pacman', 'NTVG', 2011, 'Uruguay', 'español', 5, 'digital', 'arcade', 'historia', 16);
 
-$tipo_contenido = new Fila(['','','','','Libros','','','','']);
+$tipo_contenido = new Fila(['', '', '', '', 'Libros', '', '', '', '']);
 $tipo_contenido->cambiar_a_titulo(true);
 $tabla->agregar_fila($tipo_contenido);
 
